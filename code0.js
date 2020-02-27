7cf3fc323b9b6a5be570b601ef3d8214f1a45cbd
hero <skhfld71@gmail.com>
2020-02-28 06:34:16 +0900

    first

---
+++ code0.js
@@ -0,0 +1,48 @@
+gdjs.SceneCode = {};
+gdjs.SceneCode.GDTextObjects1= [];
+gdjs.SceneCode.GDTextObjects2= [];
+gdjs.SceneCode.GDNewObjectObjects1= [];
+gdjs.SceneCode.GDNewObjectObjects2= [];
+
+gdjs.SceneCode.conditionTrue_0 = {val:false};
+gdjs.SceneCode.condition0IsTrue_0 = {val:false};
+gdjs.SceneCode.condition1IsTrue_0 = {val:false};
+
+
+gdjs.SceneCode.eventsList0xb4be0 = function(runtimeScene) {
+
+{
+
+
+gdjs.SceneCode.condition0IsTrue_0.val = false;
+{
+gdjs.SceneCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+}if (gdjs.SceneCode.condition0IsTrue_0.val) {
+gdjs.SceneCode.GDTextObjects1.createFrom(runtimeScene.getObjects("Text"));
+{gdjs.evtTools.network.jsonToVariableStructure("{" + "\"Line1\":\"ToString(Variable(score))\"," + "\"Line2\":\"감자\"," + "\"Line3\":\"고구마\"," + "\"Line4\":\"몬수터\"," + "\"Line5\":\"아자\"" + "}", runtimeScene.getVariables().get("Object"));
+}{for(var i = 0, len = gdjs.SceneCode.GDTextObjects1.length ;i < len;++i) {
+    gdjs.SceneCode.GDTextObjects1[i].setString(gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line1")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line2")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line3")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line4")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line5")));
+}
+}{gdjs.evtTools.network.sendHttpRequest("", "data.json", "", "GET", "", runtimeScene.getVariables().get("RawNews"));
+}{runtimeScene.getVariables().get("NewObject").setString("RawNews");
+}}
+
+}
+
+
+}; //End of gdjs.SceneCode.eventsList0xb4be0
+
+
+gdjs.SceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.SceneCode.GDTextObjects1.length = 0;
+gdjs.SceneCode.GDTextObjects2.length = 0;
+gdjs.SceneCode.GDNewObjectObjects1.length = 0;
+gdjs.SceneCode.GDNewObjectObjects2.length = 0;
+
+gdjs.SceneCode.eventsList0xb4be0(runtimeScene);
+return;
+
+}
+gdjs['SceneCode'] = gdjs.SceneCode;
