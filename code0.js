78e6a87f3c1cf08e4f7e0336a5b379d47e139d97
hero <skhfld71@gmail.com>
2020-02-28 06:44:47 +0900

    second

--- code0.js
+++ code0.js
@@ -18,13 +18,17 @@ gdjs.SceneCode.condition0IsTrue_0.val = false;
 {
 gdjs.SceneCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
 }if (gdjs.SceneCode.condition0IsTrue_0.val) {
+gdjs.SceneCode.GDNewObjectObjects1.createFrom(runtimeScene.getObjects("NewObject"));
 gdjs.SceneCode.GDTextObjects1.createFrom(runtimeScene.getObjects("Text"));
 {gdjs.evtTools.network.jsonToVariableStructure("{" + "\"Line1\":\"ToString(Variable(score))\"," + "\"Line2\":\"감자\"," + "\"Line3\":\"고구마\"," + "\"Line4\":\"몬수터\"," + "\"Line5\":\"아자\"" + "}", runtimeScene.getVariables().get("Object"));
 }{for(var i = 0, len = gdjs.SceneCode.GDTextObjects1.length ;i < len;++i) {
     gdjs.SceneCode.GDTextObjects1[i].setString(gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line1")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line2")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line3")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line4")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("Object").getChild("Line5")));
 }
 }{gdjs.evtTools.network.sendHttpRequest("", "data.json", "", "GET", "", runtimeScene.getVariables().get("RawNews"));
-}{runtimeScene.getVariables().get("NewObject").setString("RawNews");
+}{runtimeScene.getVariables().get("data_json").setString("RawNews");
+}{for(var i = 0, len = gdjs.SceneCode.GDNewObjectObjects1.length ;i < len;++i) {
+    gdjs.SceneCode.GDNewObjectObjects1[i].setString(gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("data_json").getChild("title")) + gdjs.evtTools.string.newLine() + gdjs.evtTools.common.getVariableString(runtimeScene.getVariables().get("data_json").getChild("content")));
+}
 }}
 
 }
